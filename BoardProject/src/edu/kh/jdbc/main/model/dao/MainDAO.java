17c955ac56ecfee8165edb34b441d33e8f6af79f
KimJongJo <desk1614@gmail.com>
2024-03-18 10:50:51 +0900

    2024 03 18 10 50 am

---
+++ BoardProject/src/edu/kh/jdbc/main/model/dao/MainDAO.java
@@ -0,0 +1,14 @@
+package edu.kh.jdbc.main.model.dao;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MainDAO {
+
+	// JDBC 객체 참조 변수
+	Statement st = null;
+	ResultSet rs = null;
+	
+	// 기본생성자 DAO 객체가 생성될 때 xml 파일 읽어와 Properties 객체 저장
+	
+}

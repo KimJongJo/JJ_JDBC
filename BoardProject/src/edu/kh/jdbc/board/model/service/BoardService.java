7d5ffcf18a4b68bdde4d3e0f898d69299a65a68c
KimJongJo <desk1614@gmail.com>
2024-03-20 17:14:54 +0900

    2024 03 20 5 14 pm

--- BoardProject/src/edu/kh/jdbc/board/model/service/BoardService.java
+++ BoardProject/src/edu/kh/jdbc/board/model/service/BoardService.java
@@ -1,10 +1,12 @@
-package edu.kh.jdbc.board.model.service;
+                                                                     package edu.kh.jdbc.board.model.service;
 
 import java.sql.Connection;
 import java.util.List;
 
 import edu.kh.jdbc.board.model.dao.BoardDAO;
+import edu.kh.jdbc.board.model.dao.CommentDAO;
 import edu.kh.jdbc.board.model.dto.Board;
+import edu.kh.jdbc.board.model.dto.Comment;
 
 import static edu.kh.jdbc.common.JDBCTemplate.*;
 
@@ -12,6 +14,7 @@ public class BoardService {
 	
 	private BoardDAO dao = new BoardDAO();
 
+	private CommentDAO commentDao = new CommentDAO();
 	
 	
 	/** 게시글 목록 조회 서비스
@@ -46,6 +49,15 @@ public class BoardService {
 		// 3. 게시글이 조회된 경우
 		if(board != null) {
 			
+			//////////////////////////////////////////////////////////////////////////
+			// ** 해당 게시글에 대한 댓글 목록 조회 DAO 호출 **
+			List<Comment> commentList = commentDao.selectCommentList(conn, input); 
+			
+			// board에 댓글 목록 세팅
+			board.setCommentList(commentList);
+			//////////////////////////////////////////////////////////////////////////
+			
+			
 			// 4. 조회수 증가
 			// 단, 게시글 작성자와 로그인한 회원이 다를 경우에만 증가
 			if( board.getMemberNo() != memberNo ) {
@@ -76,6 +88,86 @@ public class BoardService {
 		
 		return board;
 	}
+
+
+
+	/** 게시글 수정 서비스
+	 * @param boardTitle
+	 * @param string
+	 * @param boardNo
+	 * @return
+	 */
+	public int updateBoard(String boardTitle, String boardContent, int boardNo) throws Exception{
+		
+		Connection conn = getConnection();
+		
+		// 게시글 수정 DAO 호출
+		int result = dao.updateBoard(conn, boardTitle, boardContent, boardNo);
+		
+		if(result > 0) commit(conn);
+		else rollback(conn);
+		
+		close(conn);
+		
+		
+		return result;
+	}
+	
+	/** 게시글 삭제 서비스
+	 * @param boardTitle
+	 * @param boardNo
+	 * @return
+	 */
+	public int deleteBoard(int boardNo) throws Exception{
+		
+		Connection conn = getConnection();
+		
+		int result = dao.deleteBoard(conn, boardNo);
+		
+		if(result > 0) commit(conn);
+		else rollback(conn);
+		
+		close(conn);
+		
+		
+		return result;
+	}
+
+
+
+	/** 게시글 삽입 서비스
+	 * @param boardTitle
+	 * @param string
+	 * @param memberNo
+	 * @return result
+	 */
+	public int insertBoard(String boardTitle, String boardContent, int memberNo) throws Exception{
+		
+		Connection conn = getConnection();
+		
+		// 다음 게시글 번호 생성 dao 호출
+		int boardNo = dao.nextBoardNo(conn);
+		
+		// 제목, 내용, 회원번호 + 다음 게시글 번호
+		int result = dao.insertBoard(conn, boardTitle, boardContent, memberNo, boardNo);
+		
+		if(result > 0) {
+			commit(conn);
+			result = boardNo; // insert 삽입 성공 시 현재 삽입된 게시글 번호를 반환
+		}else {
+			rollback(conn);
+		}
+		
+		 close(conn);
+		
+		
+		return result; // 삽입 성공 시 현재 삽입된 게시글 번호
+						// 실패 시 0
+	}
+
+
+
+	
 	
 	
 	
